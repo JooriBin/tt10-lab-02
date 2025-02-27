c76b5a272cc91cddab9f7a539a30c9667484de9e
khouzamaa <kb4499@nyu.edu>
2025-02-27 01:49:49 -0500

    Update test.py (debug0

--- test/test.py
+++ test/test.py
@@ -19,24 +19,6 @@ async def test_project(dut):
     clock = Clock(dut.clk, 10, units="us")
     cocotb.start_soon(clock.start())
 
-here is my test .py # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
-# SPDX-FileCopyrightText: © 2024 Tiny Tapeout
-# SPDX-License-Identifier: Apache-2.0
-
-import cocotb
-from cocotb.clock import Clock
-from cocotb.triggers import ClockCycles
-
-@cocotb.test()
-async def test_project(dut):
-    dut._log.info("Start")
-
-    # Set the clock period to 10 us (100 KHz)
-    clock = Clock(dut.clk, 10, units="us")
-    cocotb.start_soon(clock.start())
-
 dut._log. info("Resetting DUT'")
 dut.ena.value = 1
 dut.ui_in.value = 0
